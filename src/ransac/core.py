08432bec59b29943cafec760450b491fe4da8221
Sébastien Gilbert <segilber@ca.ibm.com>
2021-06-17 09:56:46 -0400

    core.py: Code for ConsensusModel()

--- src/ransac/core.py
+++ src/ransac/core.py
@@ -1,5 +1,6 @@
 import numpy as np
 import abc
+import random
 
 
 class Model(abc.ABC):
@@ -11,7 +12,7 @@ class Model(abc.ABC):
         pass
 
     @abc.abstractmethod
-    def Distance(self, y1, y2):  # Compute the distance between two output variables
+    def Distance(self, y1, y2):  # Compute the distance between two output variables. Must return a float.
         pass
 
     @abc.abstractmethod
@@ -24,9 +25,40 @@ class Model(abc.ABC):
 
 
 class Modeler():
-    def __init__(self, number_of_trials, acceptable_error):
+    def __init__(self, model_class, number_of_trials, acceptable_error):
+        self.model_class = model_class
         self.number_of_trials = number_of_trials
         self.acceptable_error = acceptable_error
 
     def ConsensusModel(self, xy_tuples, **kwargs):
-        pass
+        candidate_model = self.model_class()
+        final_model = self.model_class()
+        if len(xy_tuples) < candidate_model.MinimumNumberOfDataToDefineModel():
+            raise ValueError("core.ConsensusModel(): len(xy_tuples) ({}) < candidate_model.MinimumNumberOfDataToDefineModel() ({})".format(
+                len(xy_tuples), candidate_model.MinimumNumberOfDataToDefineModel() ))
+
+        highest_sum_of_distance_inverses = -1
+        inliers_list = []
+        for trial in range(1, self.number_of_trials + 1):
+            # Randomly select the minimum number of data from the list
+            candidate_xy_list = random.sample(xy_tuples, candidate_model.MinimumNumberOfDataToDefineModel())
+            # Create a candidate model
+            candidate_model.Create(candidate_xy_list, **kwargs)
+            # Find the inliers
+            sum_of_distance_inverses = 0
+            candidate_inliers_list = []
+            for xy in xy_tuples:
+                model_prediction = candidate_model.Evaluate(xy[0])
+                model_prediction_distance = candidate_model.Distance(model_prediction, xy[1])
+                if model_prediction_distance < self.acceptable_error:
+                    sum_of_distance_inverses += (1 - model_prediction_distance/self.acceptable_error)  # A number in [0, 1]. The higher, the better.
+                    candidate_inliers_list.append(xy)
+            # Compare with the champion
+            if sum_of_distance_inverses > highest_sum_of_distance_inverses:
+                highest_sum_of_distance_inverses = sum_of_distance_inverses
+                inliers_list = candidate_inliers_list
+        # Compute the final model with the agreeing data
+        final_model.Create(inliers_list, **kwargs)
+        outliers_list = [xy for xy in xy_tuples if xy not in inliers_list]
+        return final_model, inliers_list, outliers_list
+

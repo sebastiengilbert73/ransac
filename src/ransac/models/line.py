548be07fb82f40521ea3407864bb6257fb5d062e
GMX\070038649 <segilber@ca.ibm.com>
2022-10-07 13:54:08 -0400

    src\ransac\models\line.py: Changes the linear system of equations, when more than 2 points

--- src/ransac/models/line.py
+++ src/ransac/models/line.py
@@ -29,7 +29,6 @@ class Line(ransac.Model):  # The input is a 2D point. The output is the distance
             if abs(x1[0] - x2[0]) <= self.zero_threshold and \
                     abs(x1[1] - x2[1]) <= self.zero_threshold:
                 raise ValueError("line.Line.Create(): The two points coincide at ({}, {})".format(x1[0], x1[1]))
-
             if abs(x1[0] - x2[0]) <= self.zero_threshold:  # Vertical line
                 self.rho = x1[0]
                 self.theta = 0
@@ -52,7 +51,7 @@ class Line(ransac.Model):  # The input is a 2D point. The output is the distance
                 z, residuals, rank, singular_values = np.linalg.lstsq(A, b, rcond=None)
                 self.theta = math.atan(z[0, 0])
                 self.rho = z[1, 0] * math.cos(self.theta)
-        else:
+        else:  # More than 2 points
             xs = [xy[0][0] for xy in xy_tuples]
             min_x = min(xs)
             max_x = max(xs)
@@ -60,7 +59,7 @@ class Line(ransac.Model):  # The input is a 2D point. The output is the distance
             if abs(min_x - max_x) <= self.zero_threshold:  # Vertical line
                 self.rho = min_x
                 self.theta = 0
-            else:
+            else:  # Non-vertical line
                 ys = [xy[0][1] for xy in xy_tuples]
                 min_y = min(ys)
                 max_y = max(ys)
@@ -68,21 +67,28 @@ class Line(ransac.Model):  # The input is a 2D point. The output is the distance
                 if abs(min_y - max_y) <= self.zero_threshold:  # Horizontal line
                     self.rho = min_y
                     self.theta = math.pi/2
-                else:
-                    # x_i * cos(theta) + y_i * sin(theta) = rho
-                    # x_i + y_i * tan(theta) = rho/cos(theta)  Since we know cos(theta) != 0
-                    # | y_i  -1 | |   tan(theta)   |  =  | -x_i |
-                    # | ...  ...| | rho/cos(theta) |     |  ... |
-                    A = np.zeros((len(xy_tuples), 2), dtype=float)
-                    b = np.zeros((len(xy_tuples), 1), dtype=float)
+                else:  # Non-horizontal line
+                    # | x_i    y_i   -1  | | cos(theta) | = | 0 |
+                    # | ...    ...   ... | | sin(theta) | = | 0 |
+                    # | ...    ...   ... | |    rho     |   |...|
+                    A = np.zeros((len(xy_tuples), 3), dtype=float)
                     for row in range(len(xy_tuples)):
-                        A[row, 0] = xy_tuples[row][0][1]  # y_i
-                        A[row, 1] = -1
-                        b[row, 0] = -xy_tuples[row][0][0]  # -x_i
-                    z, residuals, rank, singular_values = np.linalg.lstsq(A, b, rcond=None)
-                    self.theta = math.atan(z[0, 0])
-                    self.rho = z[1, 0] * math.cos(self.theta)
-
+                        x_i = xy_tuples[row][0][0]
+                        y_i = xy_tuples[row][0][1]
+                        A[row, 0] = x_i
+                        A[row, 1] = y_i
+                        A[row, 2] = -1
+                    # Solution to a system of homogeneous linear equations. Cf. https://stackoverflow.com/questions/1835246/how-to-solve-homogeneous-linear-equations-with-numpy
+                    e_vals, e_vecs = np.linalg.eig(np.dot(A.T, A))
+                    # Extract the eigenvector (column) associated with the minimum eigenvalue
+                    z = e_vecs[:, np.argmin(e_vals)]
+                    # Multiply by a factor such that cos^2(theta) + sin^2(theta) = 1
+                    r2 = z[0]**2 + z[1]**2
+                    if abs(r2) < self.zero_threshold:
+                        raise ValueError(f"Line.Create(): z[0]**2 + z[1]**2 ({r2}) < {self.zero_threshold}")
+                    z = z/math.sqrt(r2)
+                    self.theta = np.angle(complex(z[0], z[1]))
+                    self.rho = z[2]
 
 
     def MinimumNumberOfDataToDefineModel(self):  # The minimum number or (x, y) observations to define the model
